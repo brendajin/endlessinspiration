f011812f937cdf4e2680ac65a0d18f20091d7d07
brendajin <brendajin@gmail.com>
2013-04-14 21:33:19 -0700

    updates and bug fixes

--- EndlessInspiration.js
+++ EndlessInspiration.js
@@ -1,68 +1,82 @@
-//grab words from curated JSON file
-$.ajax({type: "GET",
+var imagesReady, wordsReady, tweetsReady;
+
+var onRequestFinished = function(){
+  if(imagesReady && wordsReady && tweetsReady){
+    allReady();
+  }
+};
+
+var allReady = function(){
+  makeNewPoster();
+};
+
+var makeNewPoster = function() {
+	var randomNumber = Math.floor(Math.random() * 20);
+	$('#mainword').html(words[randomNumber]);
+	$('#inspireimg').attr('src', images[randomNumber].media.m.replace("_m.jpg","_b.jpg"));
+	$('#posterText').html(tweets[randomNumber]);
+};
+
+
+
+$.ajax({
+	type: "GET",
 	url: "words.json",
 	dataType: "json",
-	success: function(mainword) {
-		var start = Math.floor(Math.random()*mainword.syn.length);
-		
-		document.getElementById('mainword').innerHTML = mainword.syn[start].toUpperCase();
+	success: function(words) {
+		window.words = words;
+		wordsReady = true;
+		onRequestFinished();
+	}
+});
+
+// todo: get the two question marks out of this url
+// todo: move the jsoncallback param into the data hash
+$.getJSON("http://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?", {
+	tags: "animal,landscape,cute,bleak",
+	tagmode: "any",
+	format: "json",
+	license: "7",
+	privacy_filter: "1",
+	safe_search: "1",
+	content_type: "1",
+	media: "photos",
+	per_page:"60",
+	page: "1"
+}, function(data) {
+	window.images = data.items;
+	imagesReady = true;
+	onRequestFinished();
+});
+
+
+$.ajax({
+	url: "http://search.twitter.com/search.json?",
+	type: "GET",
+	dataType: "jsonp",
+	data: { 'q': "haiku OR nature OR water OR bridge OR STONE OR rock OR volcano OR reach OR try OR never OR always OR forever OR sometimes OR fire OR earth OR wind",
+		'sensitive': false,
+		'truncated': false,
+		'lang': 'en',
+		'possibly_sensitive': false,
+		'page': 1,
+		'rpp': 100},
+	success: function(json) {
+		var results = json.results;
+
+		window.tweets = [];
+		for (var i =0; i < results.length; i++) {
+			if (results[i].text.indexOf("@") == -1 && results[i].text.indexOf("#") == -1 && results[i].text.indexOf("://") == -1 && results[i].text.indexOf("\"") == -1) {
+				tweets.push(results[i].text.toUpperCase());
+			}
+		}
+	tweetsReady = true;
+	onRequestFinished();
 	}
 });
 
 
 
-//flickr search API
-//http://www.flickr.com/search/?q=animal+OR+landscape&l=deriv&ss=0&ct=0&mt=photos&w=all&adv=1
-$.getJSON("http://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?",
-	{
-		tags: "animal,landscape,cute,bleak",
-		tagmode: "any",
-		format: "json",
-		license: "7",
-		privacy_filter: "1",
-		safe_search: "1",
-		content_type: "1",
-		media: "photos",
-		per_page:"20",
-		page: "1",
-	},
-
-	function(data) {
-		var randImg = Math.floor(Math.random()*10);
-		// var testImg = new Image();
-		// testImg.src=data.items[randImg].media.m;
-		// console.log(testImg.width);
-
-		document.getElementById('inspireimg').src = data.items[randImg].media.m.replace("_m.jpg","_b.jpg");
-	});
-
-
-
-//Twitter search API
-function onLoad(json) {
-	var results = json.results;
-	var tweetsArray = [];
-
-	// for (var i = 0; i < results.length;i++){
-	// 	if (results[i].text.indexOf("@") == -1 && results[i].text.indexOf("#") == -1 && results[i].text.indexOf("://") == -1 &&results[i].text.indexOf("\"") == -1) {
-	// 		document.getElementById('posterText').innerHTML = results[i].text;
-	// 	}
-	// }
-
-	for (var i =0; i < results.length; i++) {
-		if (results[i].text.indexOf("@") == -1 && results[i].text.indexOf("#") == -1 && results[i].text.indexOf("://") == -1 && results[i].text.indexOf("\"") == -1) {
-			console.log(results[i].text);
-			tweetsArray.push(String(results[i].text));
-		}
-	}
-	document.getElementById('posterText').innerHTML = tweetsArray[Math.floor(Math.random()*tweetsArray.length)];
-}
-
-function fetchJSON(query) {
-	var script = document.createElement('script');
-	//%20%3A results in positive sentiment
-	script.src = 'http://search.twitter.com/search.json?q=' + query + '&page=1&rpp=100&callback=onLoad&lang=en&truncated=false&possibly_sensitive=false%20%3A)';
-	document.getElementsByTagName('body')[0].appendChild(script);
-}
-
-fetchJSON("haiku OR nature OR water OR bridge OR STONE or rock OR volcano OR reach OR try OR never OR always OR forever OR sometimes OR fire OR earth OR wind");
+setInterval(function(){
+  makeNewPoster();
+}, 15000);
